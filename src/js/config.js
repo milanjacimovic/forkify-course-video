a4674213ac13b3e76ee0a172c124f1effa201936
milanjacimovic <jacimovicmilanfon@gmail.com>
2021-08-26 19:10:15 +0200

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
+export const TIMEOUT_SECONDS = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '833048e5-72fb-4404-a08a-9b02f482ac3d';
+export const MODAL_CLOSE_SEC = 2.5;
